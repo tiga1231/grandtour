31680b8760b01067ee05405797ac01643ca962c2
jack <jack@email.com>
2018-10-10 14:14:27 -0700

    lovr pause grandtour

--- lovr/gt/main.lua
+++ lovr/gt/main.lua
@@ -15,29 +15,52 @@ local maxRange = 0.5
 local axisData = linalg.mul(linalg.eye(10,10), maxRange)
 local axis
 
+local nepoch = 100
+local npoint = 1000
+local ndim = 10
+isGrandTourPlaying = true
+local gt = GrandTour:new(ndim,0.4)
+_G.touch0 = {left={0,0}, right={0,0}}
+
 function lovr.load()
-	dataRaw = assert(io.open('gt/data/fashion-mnist/softmax.bin', 'rb'))
 	labelRaw = assert(io.open('gt/data/fashion-mnist/labels.bin', 'rb'))
-	dataTensor = {}
-	for epoch=1,100 do
-		dataTensor[epoch] = {}
-		for i=1,1000 do
-			dataTensor[epoch][i] = {}
-			for j=1,10 do
+	labelPairs = {}
+	for i=1,npoint do
+		local l = labelRaw:read(1)
+		labelPairs[i] = {i, utils.toInt(l)}
+	end
+	table.sort(labelPairs, function(a,b) return a[2] < b[2] end)
+	labels = {}
+	for i=1,npoint do
+		labels[i] = labelPairs[i][2]
+	end
+
+	dataRaw = assert(io.open('gt/data/fashion-mnist/softmax.bin', 'rb'))
+	local dataTensor0 = {}
+	for epoch=1,nepoch do
+		dataTensor0[epoch] = {}
+		for i=1,npoint do
+			dataTensor0[epoch][i] = {}
+			for j=1,ndim do
 				local d = dataRaw:read(4)
 				local f = utils.toFloat(d)
 				if math.abs(f) > dmax then 
 					dmax = f
 				end
-				dataTensor[epoch][i][j] = f
+				dataTensor0[epoch][i][j] = f
 			end
 		end
 	end
-	labels = {}
-	for i=1,1000 do
-		local l = labelRaw:read(1)
-		labels[i] = utils.toInt(l)
+
+	dataTensor = {}
+	for epoch=1,nepoch do
+		dataTensor[epoch] = {}
+		for i=1,npoint do
+			dataTensor[epoch][i] = {}
+				dataTensor[epoch][i] = dataTensor0[epoch][labelPairs[i][1]]
+		end
 	end
+	
 	-- shader = lovr.graphics.newShader([[
 	-- out vec3 vNormal; // This gets passed to the fragment shader
 	-- vec4 position(mat4 projection, mat4 transform, vec4 vertex) {
@@ -52,15 +75,41 @@ function lovr.load()
 	-- }
 	-- ]])
 	-- lovr.graphics.setShader(shader)
+	-- lovr.graphics.setBlendMode('alpha', 'premultiplied')
+
 end
 
-local gt = GrandTour:new(10,0.3)
 function lovr.update(dt)
-	gt:tick(dt)
+	if isGrandTourPlaying then
+		gt:tick(dt)
+	end
 	local controllers = lovr.headset.getControllers()
 	if controllers ~=nil then
 		for i,controller in ipairs(controllers) do
-			if handlings[controller:getHand()] ~= nil then
+			local hand = controller:getHand()
+			local tx = controller:getAxis('touchx')
+			local ty = controller:getAxis('touchy')
+			local angle = math.atan(math.abs(ty/tx))
+
+			if tx>0 and ty<0 then
+				angle = 2*math.pi - angle
+			elseif tx<0 and ty>0 then
+				angle = math.pi - angle
+			elseif tx<0 and ty<0 then
+				angle = math.pi + angle
+			end
+
+			local e = math.ceil(angle/(math.pi*2) * nepoch)
+			if tostring(e)~='nan' then
+				epoch = e
+			end
+			msg = tostring(epoch)
+
+			-- touch0[hand] = {tx, ty}
+			-- if angle > angle0 then
+			-- end
+
+			if handlings[hand] ~= nil then
 				local cx, cy, cz = controller:getPosition()
 				gt.matrix[handlings[controller:getHand()]][1] = (cx-dx)/maxRange
 				gt.matrix[handlings[controller:getHand()]][2] = (cy-dy)/maxRange
@@ -89,41 +138,53 @@ function lovr.draw()
 	-- msg = tostring(dx+axis[1][1]).. ', \n'..tostring(dx+axis[1][2])..', \n'..tostring(dz+axis[1][3])..'\n\n'..tostring(dx+axis[2][1]).. ', \n'..tostring(dx+axis[2][2])..', \n'..tostring(dz+axis[2][3])
 
 
-	for i=1,10 do
+	for i=1,ndim do
 		local q = axis[i]
 		local c = utils.baseColors[i]
 		lovr.graphics.setColor(c[1], c[2], c[3], 1.0)
 		lovr.graphics.sphere(q[1]+dx, q[2]+dy, q[3]+dz, 0.02)
-		lovr.graphics.setColor(c[1], c[2], c[3], 1.01)
+		lovr.graphics.setColor(c[1], c[2], c[3], 1.0)
 		lovr.graphics.cylinder(dx, dy, dz, q[1]+dx, q[2]+dy, q[3]+dz, 0.002,0.002, false, 6)
 
 	end
 
 
 	local points = gt:project(dataTensor[epoch])
-	for i=1,1000 do
+	local c0 = nil
+	for i=1,npoint do
 		local point = points[i]
 		local c = utils.baseColors[labels[i]+1]
-		lovr.graphics.setColor(c[1], c[2], c[3])
+		if c~=c0 then
+			lovr.graphics.setColor(c[1], c[2], c[3])
+			c0 = c
+		end
 		lovr.graphics.sphere(point[1]/dmax*maxRange+dx, point[2]/dmax*maxRange+dy, point[3]/dmax*maxRange+dz, 0.003)
 	end
 	lovr.graphics.print(msg, 0, 1,-5)
 end
 
+function nextEpoch()
+	epoch = epoch + 1
+	if epoch > nepoch then
+		epoch = 1
+	end
+end
+function prevEpoch()
+	epoch = epoch - 1
+	if epoch < 1 then
+		epoch = nepoch
+	end
+end
 function lovr.controllerpressed(controller0, button)
 	if button == 'touchpad' then
 		local x = controller0:getAxis('touchx')
 		local y = controller0:getAxis('touchy')
-		if x>0 then
-			epoch = epoch + 1
-			if epoch > 100 then
-				epoch = 1
-			end
+		if math.sqrt(x^2+y^2)<0.6 then
+			isGrandTourPlaying = not isGrandTourPlaying
+		elseif x>0 then
+			nextEpoch()
 		else
-			epoch = epoch - 1
-			if epoch < 1 then
-				epoch = 100
-			end
+			prevEpoch()
 		end
 	elseif button == 'trigger' then
 		local cx,cy,cz = controller0:getPosition()
